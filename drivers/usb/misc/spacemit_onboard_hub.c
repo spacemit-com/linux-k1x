3af0ef0caed973d953c791f85a5c1411c82bbbbb
Pan Junzhong <junzhong.pan@spacemit.com>
2024-04-01 20:09:44 +0800

    usb: spacemit_onboard_hub: fix bug caused by no delay before enable vbus, support debugfs
    Change-Id: Ie091132052ec774cc56caab82e34d2b0536af4a2

--- drivers/usb/misc/spacemit_onboard_hub.c
+++ drivers/usb/misc/spacemit_onboard_hub.c
@@ -17,30 +17,139 @@
 #include <linux/of_address.h>
 #include <linux/gpio/consumer.h>
 
-struct spacemit_hub_priv {
-	struct gpio_descs *gpios;
-};
+#include "spacemit_onboard_hub.h"
+
+#define DRIVER_VERSION "v1.0.0"
+
+static void spacemit_hub_enable(struct spacemit_hub_priv *spacemit, bool on)
+{
+	unsigned i;
+	int active_val = spacemit->hub_gpio_active_low ? 0 : 1;
+	dev_dbg(spacemit->dev, "do hub enable %s\n", on ? "on" : "off");
+
+	if (on) {
+		for (i = 0; i < spacemit->hub_gpios->ndescs; i++) {
+			gpiod_set_value(spacemit->hub_gpios->desc[i],
+					active_val);
+			if (spacemit->hub_inter_delay_ms) {
+				msleep(spacemit->hub_inter_delay_ms);
+			}
+		}
+	} else {
+		for (i = spacemit->hub_gpios->ndescs; i > 0; --i) {
+			gpiod_set_value(spacemit->hub_gpios->desc[i - 1],
+					!active_val);
+			if (spacemit->hub_inter_delay_ms) {
+				msleep(spacemit->hub_inter_delay_ms);
+			}
+		}
+	}
+	spacemit->is_hub_on = on;
+}
+
+static void spacemit_hub_vbus_enable(struct spacemit_hub_priv *spacemit,
+					 bool on)
+{
+	unsigned i;
+	int active_val = spacemit->vbus_gpio_active_low ? 0 : 1;
+
+	dev_dbg(spacemit->dev, "do hub vbus on %s\n", on ? "on" : "off");
+	if (on) {
+		for (i = 0; i < spacemit->vbus_gpios->ndescs; i++) {
+			gpiod_set_value(spacemit->vbus_gpios->desc[i],
+					active_val);
+			if (spacemit->vbus_inter_delay_ms) {
+				msleep(spacemit->vbus_inter_delay_ms);
+			}
+		}
+	} else {
+		for (i = spacemit->vbus_gpios->ndescs; i > 0; --i) {
+			gpiod_set_value(spacemit->vbus_gpios->desc[i - 1],
+					!active_val);
+			if (spacemit->vbus_inter_delay_ms) {
+				msleep(spacemit->vbus_inter_delay_ms);
+			}
+		}
+	}
+	spacemit->is_vbus_on = on;
+}
+
+static void spacemit_hub_configure(struct spacemit_hub_priv *spacemit, bool on)
+{
+	dev_dbg(spacemit->dev, "do hub configure %s\n", on ? "on" : "off");
+	mutex_lock(&spacemit->hub_mutex);
+	if (on) {
+		spacemit_hub_enable(spacemit, true);
+		if (spacemit->vbus_delay_ms && spacemit->vbus_gpios->ndescs) {
+			msleep(spacemit->vbus_delay_ms);
+		}
+		spacemit_hub_vbus_enable(spacemit, true);
+	} else {
+		spacemit_hub_vbus_enable(spacemit, false);
+		if (spacemit->vbus_delay_ms && spacemit->vbus_gpios->ndescs) {
+			msleep(spacemit->vbus_delay_ms);
+		}
+		spacemit_hub_enable(spacemit, false);
+	}
+	mutex_unlock(&spacemit->hub_mutex);
+}
+
+static void spacemit_read_u32_prop(struct device *dev, const char *name,
+				   u32 init_val, u32 *pval)
+{
+	if (device_property_read_u32(dev, name, pval))
+		*pval = init_val;
+	dev_dbg(dev, "hub %s, delay: %u ms\n", name, *pval);
+}
 
 static int spacemit_hub_probe(struct platform_device *pdev)
 {
 	struct spacemit_hub_priv *spacemit;
-	unsigned i;
+	struct device *dev = &pdev->dev;
+
+	dev_info(&pdev->dev, "%s\n", DRIVER_VERSION);
 
 	spacemit = devm_kzalloc(&pdev->dev, sizeof(*spacemit), GFP_KERNEL);
 	if (!spacemit)
 		return -ENOMEM;
 
-	spacemit->gpios = devm_gpiod_get_array(&pdev->dev, "hub", GPIOD_OUT_HIGH);
-	if (IS_ERR(spacemit->gpios)) {
+	spacemit_read_u32_prop(dev, "hub_inter_delay_ms", 0,
+				   &spacemit->hub_inter_delay_ms);
+	spacemit_read_u32_prop(dev, "vbus_inter_delay_ms", 0,
+				   &spacemit->vbus_inter_delay_ms);
+	spacemit_read_u32_prop(dev, "vbus_delay_ms", 10,
+				   &spacemit->vbus_delay_ms);
+
+	spacemit->hub_gpio_active_low =
+		device_property_read_bool(dev, "hub_gpio_active_low");
+	spacemit->vbus_gpio_active_low =
+		device_property_read_bool(dev, "vbus_gpio_active_low");
+
+	spacemit->hub_gpios = devm_gpiod_get_array(
+		&pdev->dev, "hub",
+		spacemit->hub_gpio_active_low ? GPIOD_OUT_HIGH : GPIOD_OUT_LOW);
+	if (IS_ERR(spacemit->hub_gpios)) {
 		dev_err(&pdev->dev, "failed to retrieve hub-gpios from dts\n");
-		return PTR_ERR(spacemit->gpios);
+		return PTR_ERR(spacemit->hub_gpios);
 	}
-	for (i = 0; i < spacemit->gpios->ndescs; i++) {
-		gpiod_set_value(spacemit->gpios->desc[i], 1);
+
+	spacemit->vbus_gpios = devm_gpiod_get_array(
+		&pdev->dev, "vbus",
+		spacemit->vbus_gpio_active_low ? GPIOD_OUT_HIGH : GPIOD_OUT_LOW);
+	if (IS_ERR(spacemit->vbus_gpios)) {
+		dev_err(&pdev->dev, "failed to retrieve vbus-gpios from dts\n");
+		return PTR_ERR(spacemit->vbus_gpios);
 	}
-	dev_info(&pdev->dev, "onboard usb hub driver probed, hub configured\n");
 
 	platform_set_drvdata(pdev, spacemit);
+	spacemit->dev = &pdev->dev;
+	mutex_init(&spacemit->hub_mutex);
+
+	spacemit_hub_configure(spacemit, true);
+
+	dev_info(&pdev->dev, "onboard usb hub driver probed, hub configured\n");
+
+	spacemit_hub_debugfs_init(spacemit);
 
 	return 0;
 }
@@ -48,12 +157,10 @@ static int spacemit_hub_probe(struct platform_device *pdev)
 static int spacemit_hub_remove(struct platform_device *pdev)
 {
 	struct spacemit_hub_priv *spacemit = platform_get_drvdata(pdev);
-	unsigned i;
 
-	for (i = 0; i < spacemit->gpios->ndescs; i++) {
-		gpiod_set_value(spacemit->gpios->desc[i], 0);
-	}
-
+	spacemit_hub_configure(spacemit, false);
+	debugfs_remove(debugfs_lookup(dev_name(&pdev->dev), usb_debug_root));
+	mutex_destroy(&spacemit->hub_mutex);
 	dev_info(&pdev->dev, "onboard usb hub driver exit, disable hub\n");
 	return 0;
 }
