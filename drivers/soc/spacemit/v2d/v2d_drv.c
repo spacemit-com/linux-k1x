905af130154fd768fd7551a87a8b9eec4540f89a
zhangmeng <zhangmeng.kevin@spacemit.com>
2024-11-26 10:11:48 +0800

    spacemit: v2d: modify prototype of device remove function
    Change-Id: Ie5e7790496af41c9cc270fa34c123a7b1339aa84

--- drivers/soc/spacemit/v2d/v2d_drv.c
+++ drivers/soc/spacemit/v2d/v2d_drv.c
@@ -20,6 +20,8 @@
 #include <linux/dma-buf.h>
 #include <linux/highmem.h>
 
+MODULE_IMPORT_NS(DMA_BUF);
+
 #define  V2D_DRV_NAME		"spacemit_v2d"
 struct v2d_info *v2dInfo;
 
@@ -898,7 +900,7 @@ err_misc:
 	return rval;
 }
 
-static int v2d_remove(struct platform_device *pdev)
+static void v2d_remove(struct platform_device *pdev)
 {
 	struct v2d_info *info = platform_get_drvdata(pdev);
 	struct device *dev = &info->pdev->dev;
@@ -931,8 +933,6 @@ static int v2d_remove(struct platform_device *pdev)
 	}
 
 	v2dInfo = NULL;
-
-	return 0;
 }
 
 static const struct of_device_id v2d_drv_match_table[] = {
